899dd035d96db1195ee701cd0a7d955853a5eca3
cqx <cqx>
2021-06-19 15:28:31 +0800

    worked find unique kmer

---
+++ find_unique/check.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <cstdint>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    map<string, string> true_answer_;
+
+    fstream f(argv[1], ios::in);
+
+    //fstream f_("argv1_file.txt", ios::out);
+
+    if(!f)
+    {
+        cerr << "open " << argv[1] << " fail\n";
+        exit(0);
+    }
+    while(!f.eof())
+    {
+        char buf[256] = {0};
+        f.getline(buf, 256);
+        char buf_[256] = {0};
+        f.getline(buf_, 256);
+        string id(buf);
+        string kmer(buf_);
+        //f_ << id << endl;
+        //f_ << kmer << endl;
+        //exit(0);
+        if(kmer.size() != 0)
+        {
+            if(true_answer_.find(kmer) != true_answer_.end())
+            {
+                if(true_answer_[kmer] != id)
+                    true_answer_[kmer] = "F";
+            }
+            else
+            {
+                true_answer_[kmer] = id;
+            }
+        }
+    }
+
+    map<string, string> true_answer;
+    for(auto i = true_answer_.begin(); i != true_answer_.end(); i++)
+    {
+        if(i->second != "F")
+            true_answer[i->first] = i->second;
+    }
+
+    f.close();
+    //f_.close();
+
+    cerr << "read complete\n";
+
+    map<string, string> check_answer;
+
+    fstream f1(argv[2], ios::in);
+    //fstream f1_("argv2_file.txt", ios::out);
+    if(!f1)
+    {
+        cerr << "open " << argv[2] << " fail\n";
+        exit(0);
+    }
+
+    //int count_ = 0;
+
+    while(!f1.eof())
+    {
+        char buf[256] = {0};
+        f1.getline(buf, 256);
+        char buf_[256] = {0};
+        f1.getline(buf_, 256);
+        string id(buf);
+        string kmer(buf_);
+
+        //f1_ << id << endl;
+        //f1_ << kmer << endl;
+        //count_++;
+        //if(count_ == 20)
+        //    exit(0);
+
+        if(kmer.size() != 0)
+        {
+            check_answer[kmer] = id;
+        }
+    }
+    f1.close();
+    //f1_.close();
+
+    cerr << "read complete\n";
+
+    //check true_answer in check_answer
+    cout << "true_answer in check_answer\n";
+    for(auto i = true_answer.begin(); i != true_answer.end(); i++)
+    {
+        if(check_answer.find(i->first) == check_answer.end())
+            cout << i->first << ":" << i->second << endl;
+    }
+
+    cerr << "compare complete\n";
+
+    cout << "check_answer in true_answer\n";
+    for(auto i = check_answer.begin(); i != check_answer.end(); i++)
+    {
+        if(true_answer.find(i->first) == true_answer.end())
+            cout << i->first << ":" << i->second << endl;
+    }
+    
+    cerr << "compare complete\n";
+
+    return 0;
+}
+
---
+++ find_unique/test1.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+#include <string>
+#include <atomic>
+#include "unique_kmer.h"
+
+using namespace std;
+
+atomic_int file_index(-1);
+
+void thread_fun(vector<string> *file_names, vector<string> *v_kinds, Write_file *w_file)
+{
+    while(true)
+    {
+        int i = ++file_index;
+        cout << i << endl;
+        if(i >= 512)
+            break;
+        string s = (*file_names)[i];
+        get_unique_kmer(&s, 25, *v_kinds, *w_file);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    vector<string> v(300);
+    for(int i = 0; i < 300; i++)
+        v[i] = to_string(i);
+    //void get_unique_kmer(const string *file_name, int kmer_len, vector<string> &ids, Write_file &w_file)
+
+    //string file_name("test.data");
+    //string file_name = "/home/old_home/haoz/workspace/KMC/tests/tmp/kmc_00511.bin";
+    //string file_name(argv[1]);
+    //fstream f(file_name, ios::in);
+    //if(!f)
+    //{
+    //    cerr << "open file " << file_name << " fail\n";
+    //    exit(0);
+    //}
+    //vector<string> file_vectors;
+    //while(!f.eof())
+    //{
+    //    char buf[256];
+    //    f.getline(buf, 256);
+    //    string s(buf);
+    //    if(s.size() != 0)
+    //        file_vectors.push_back(s);
+    //}
+
+    //cout << file_vectors.size() << endl;
+
+    Write_file w_file("outfile.txt", 1);
+    //thread t(std::ref(w_file));
+
+    //vector<thread> threads;
+
+    //for(int i = 0; i < 20; i++)
+    //    threads.emplace_back(thread_fun, &file_vectors, &v, &w_file);
+
+    //for(int i = 0; i < 20; i++)
+    //    threads[i].join();
+
+    //t.join();
+      //void thread_fun(vector<string> *file_names, vector<string> *v_kinds, Write_file *w_file)
+      //
+    //!!!!!!!!!!!!!!!!!!!!!!!111
+
+    //for(int i = 0; i < file_vectors.size(); i++)
+    //{
+    //    threads.emplace_back(get_unique_kmer, &file_vectors[i], 25, std::ref(v), std::ref(w_file));
+    //}
+
+    //for(int i = 0; i < file_vectors.size(); i++)
+    //    threads[i].join();
+    //t.join();
+
+
+    string file_name(argv[1]);
+    thread t1(get_unique_kmer, &file_name, 25, std::ref(v), std::ref(w_file));
+    thread t2(std::ref(w_file));
+    t1.join();
+    t2.join();
+}
+
